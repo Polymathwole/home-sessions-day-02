555c94fe8724a0acb06a60a62f47d8e38e728e40
Oluwaremi Oluwole <polymathwole@gmail.com>
2017-05-12 07:27:30 +0100

    Add feature to allow user convert to other currencies

--- exchange-rate-app/currencyapp.js
+++ exchange-rate-app/currencyapp.js
@@ -1,7 +1,23 @@
-
+const program = require('commander');
 const Agent = require('node-rest-client').Client;
 const app = new Agent();
 
+console.log("AED (UAE Dirham)\nBTC (Bitcoin)\nCAD (Canadian Dollar)\nCNY (Chinese Yuan)\nEUR (Euro)\nGBP (British Pound Sterling)\nJPY (Japanese Yen)\nNGN (Nigerain Naira)\nSAR (Saudi Riyal)\nSEK (Swedish Krona)\nZAR (South African Rand)\nUSD (United States Dollar)");
+
+program.version("0.0.1").
+command('exchnairatoother <amt>').
+description("Enter the amount in Naira you want to convert and the currency name as specified above").
+option('-i,--input <curr>','3 letter abbreviation of currency to convert to as shown above')
+.action((amt,curr)=>
+{
+getData(amt,curr);
+}
+);
+
+program.parse(process.argv);
+
+const getData=(amount,currency)=>
+{
 app.get("http://apilayer.net/api/live?access_key=420063ab159087dfcb8b38da4a9c5f5f&currencies=AED,BTC,CAD,CNY,EUR,GBP,JPY,NGN,SAR,SEK,ZAR",
 (data, response)=>//function used to parse data 
 {
@@ -10,18 +26,14 @@ app.get("http://apilayer.net/api/live?access_key=420063ab159087dfcb8b38da4a9c5f5
     let currentdatetime=new Date(utctime*1000);
 
     console.log("\nTimestamp: " + currentdatetime.toString()+"\n");
-    console.log("AED (UAE Dirham)\nBTC (Bitcoin)\nCAD (Canadian Dollar)\nCNY (Chinese Yuan)\nEUR (Euro)\nGBP (British Pound Sterling)\nJPY (Japanese Yen)\nNGN (Nigerain Naira)\nSAR (Saudi Riyal)\nSEK (Swedish Krona)\nZAR (South African Rand)\nUSD (United States Dollar)\n");//currencies legend
 
-    console.log('1 AED = '+(quotes.USDNGN/quotes.USDAED).toFixed(4)+" NGN, 1 NGN = "+(quotes.USDAED/quotes.USDNGN).toFixed(7)+" AED");
-    console.log('1 BTC = '+(quotes.USDNGN/quotes.USDBTC).toFixed(4)+" NGN, 1 NGN = "+(quotes.USDBTC/quotes.USDNGN).toFixed(7)+" BTC");
-    console.log('1 CAD = '+(quotes.USDNGN/quotes.USDCAD).toFixed(4)+" NGN, 1 NGN = "+(quotes.USDCAD/quotes.USDNGN).toFixed(7)+" CAD");
-    console.log('1 CNY = '+(quotes.USDNGN/quotes.USDCNY).toFixed(4)+" NGN, 1 NGN = "+(quotes.USDCNY/quotes.USDNGN).toFixed(7)+" CNY");
-    console.log('1 EUR = '+(quotes.USDNGN/quotes.USDEUR).toFixed(4)+" NGN, 1 NGN = "+(quotes.USDEUR/quotes.USDNGN).toFixed(7)+" EUR");
-    console.log('1 GBP = '+(quotes.USDNGN/quotes.USDGBP).toFixed(4)+" NGN, 1 NGN = "+(quotes.USDGBP/quotes.USDNGN).toFixed(7)+" GBP");
-    console.log('1 JPY = '+(quotes.USDNGN/quotes.USDJPY).toFixed(4)+" NGN, 1 NGN = "+(quotes.USDJPY/quotes.USDNGN).toFixed(7)+" JPY");
-    console.log('1 SAR = '+(quotes.USDNGN/quotes.USDSAR).toFixed(4)+" NGN, 1 NGN = "+(quotes.USDSAR/quotes.USDNGN).toFixed(7)+" SAR");
-    console.log('1 SEK = '+(quotes.USDNGN/quotes.USDSEK).toFixed(4)+" NGN, 1 NGN = "+(quotes.USDSEK/quotes.USDNGN).toFixed(7)+" SEK");
-    console.log('1 ZAR = '+(quotes.USDNGN/quotes.USDZAR).toFixed(4)+" NGN, 1 NGN = "+(quotes.USDZAR/quotes.USDNGN).toFixed(7)+" ZAR");
-    console.log('1 USD = '+(quotes.USDNGN).toFixed(4)+" NGN, 1 NGN = "+(1/quotes.USDNGN).toFixed(7)+" USD");
-    
+    for (let val of quotes)
+    {
+        if ("USD"+currency===val)
+            {
+                let amtval = (amount*(quotes.USDAED/quotes.USDNGN)).toFixed(7);
+                console.log(amount + " NGN= " + amtval+ currency);
+            }
+    }
 });
+}
